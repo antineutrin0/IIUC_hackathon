2084188408b774cd218f9c38b11f05ca03036751
Tushar Das <tushar23das@gmail.com>
2025-11-14 16:26:19 +0600

    roadmap implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,82 +1,91 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from './pages/Home'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import VerifyEmail from './pages/VerifyEmail'
-import Verify from './pages/Verify'
-import Navbar from './components/Navbar'
-import ForgotPassword from './pages/ForgotPassword'
-import VerifyOTP from './pages/VerifyOTP'
-import ChangePassword from './pages/ChangePassword'
-import JobBoardHome from './components/home/JobBoardHome'
-import ResourcePage from './components/resource/ResourcePage'
-import UserProfilePage from './components/profile/UserProfilePage'
-import Recruiter from './components/recruiter/Recruiter'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import VerifyEmail from "./pages/VerifyEmail";
+import Verify from "./pages/Verify";
+import Navbar from "./components/Navbar";
+import ForgotPassword from "./pages/ForgotPassword";
+import VerifyOTP from "./pages/VerifyOTP";
+import ChangePassword from "./pages/ChangePassword";
+import JobBoardHome from "./components/home/JobBoardHome";
+import ResourcePage from "./components/resource/ResourcePage";
+import UserProfilePage from "./components/profile/UserProfilePage";
+import Recruiter from "./components/recruiter/Recruiter";
+import RoadmapDashboard from "./pages/RoadmapDashboard";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <><Navbar /><Home /></>
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
   },
   {
-   path:'/home',
-   element:<JobBoardHome></JobBoardHome>
+    path: "/home",
+    element: <JobBoardHome></JobBoardHome>,
   },
   {
-    path: '/resources',
-    element:<ResourcePage></ResourcePage>,
+    path: "/resources",
+    element: <ResourcePage></ResourcePage>,
   },
   {
-    path:'/user/profile',
-    element:<UserProfilePage></UserProfilePage>
+    path: "/user/profile",
+    element: <UserProfilePage></UserProfilePage>,
   },
   //  {
   //   path: '/recruiter',
   //   element: <Recruiter></Recruiter>,
   // },
   {
-    path: '/signup',
-    element: <Signup />
+    path: "/signup",
+    element: <Signup />,
   },
   {
-    path: '/verify',
-    element: <VerifyEmail />
+    path: "/verify",
+    element: <VerifyEmail />,
   },
   {
-    path: '/verify/:token',
-    element: <Verify />
+    path: "/verify/:token",
+    element: <Verify />,
   },
   {
-    path: '/login',
-    element: <Login />
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: '/forgot-password',
-    element: <ForgotPassword />
+    path: "/forgot-password",
+    element: <ForgotPassword />,
   },
   {
-    path: '/verify-otp/:email',
-    element: <VerifyOTP />
+    path: "/verify-otp/:email",
+    element: <VerifyOTP />,
   },
   {
-    path: '/change-password/:email',
-    element: <ChangePassword />
-  }
+    path: "/change-password/:email",
+    element: <ChangePassword />,
+  },
+  { 
+    path: "/roadmap", 
+    element: <RoadmapDashboard /> 
+  },
   // {
   //   path: '/profile',
   //   element: <UserProfile />
   // }
   // ,
-  
-])
+]);
 
 const App = () => {
   return (
     <div>
       <RouterProvider router={router} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/api/roadmapApi.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export async function fetchRoadmap(userId, jobId) {
+  try {
+    const response = await axios.post("http://localhost:8000/roadmap", {
+      userId,
+      jobId,
+    });
+
+    return response.data; // Axios returns parsed JSON automatically
+  } catch (error) {
+    console.error("Error fetching roadmap:", error);
+    throw new Error("Failed to fetch roadmap");
+  }
+}
